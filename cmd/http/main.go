801267726e00c94f11bd6e14a6ebc38665e3e617
navneetshukl <navneetshukla824@gmail.com>
2025-05-10 10:15:18 +0530

    wip:added dockerfile and some documentation

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -11,7 +11,6 @@ import (
 )
 
 func main() {
-	// Load environment variables from .env file
 	err := godotenv.Load("cmd/http/.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -19,8 +18,6 @@ func main() {
 
 	appToken := os.Getenv("SLACK_APP_TOKEN")
 	botToken := os.Getenv("SLACK_BOT_TOKEN")
-
-	// Slack API and socketmode client setup
 	client := slack.New(botToken, slack.OptionDebug(true), slack.OptionAppLevelToken(appToken))
 	socketClient := socketmode.New(
 		client,
@@ -28,14 +25,24 @@ func main() {
 	)
 
 	go func() {
+		idx:=0
 		for evt := range socketClient.Events {
 			switch evt.Type {
 			case socketmode.EventTypeInteractive:
 				log.Println("Interactive event received")
 			case socketmode.EventTypeEventsAPI:
 				event, ok := evt.Data.(slackevents.EventsAPIEvent)
+				log.Println("##############################################")
+				log.Println("Event is ", event)
+				log.Println("Inner Event is ",event.InnerEvent)
+				log.Println("Inner Event type is ",event.InnerEvent.Type)
+				log.Println("Inner Event data is ",event.InnerEvent.Data)
+				log.Println("Event Type is ",event.Type)
+				log.Println("idx is ",idx)
+				log.Println("##############################################")
+				idx++
+
 				if !ok {
-					log.Printf("Ignored %+v\n", evt)
 					continue
 				}
 				socketClient.Ack(*evt.Request)
@@ -43,25 +50,36 @@ func main() {
 				if event.Type == slackevents.CallbackEvent {
 					switch ev := event.InnerEvent.Data.(type) {
 					case *slackevents.AppMentionEvent: //bot message
-						log.Printf("App mention received: %s\n", ev.Text) 
-						log.Println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-						client.PostMessage(ev.Channel, slack.MsgOptionText("Bot Message: "+ev.Text, false))
-					case *slackevents.MessageEvent:
-						
-						if ev.SubType == "" { // regular message
-							log.Printf("Message received: %s\n", ev.Text)
+						AppMentionEvent(client, ev)
 
-							
-							client.PostMessage(ev.Channel, slack.MsgOptionText("Simple Message: "+ev.Text, false))
-						}
+						//case *slackevents.MessageEvent:
+
+						//if ev.SubType == "" { // regular message
+						//	log.Printf("Message received: %s\n", ev.Text)
+
+						//client.PostMessage(ev.Channel, slack.MsgOptionText("Simple Message: "+ev.Text, false))
+						//	}
 					}
 				}
 			default:
-				log.Printf("Ignored event type: %s\n", evt.Type)
+				//log.Printf("Ignored event type: %s\n", evt.Type)
 			}
 		}
 	}()
 
 	log.Println("Slack bot is running in Socket Mode...")
 	socketClient.Run()
+}
+
+func AppMentionEvent(client *slack.Client, event *slackevents.AppMentionEvent) {
+
+	// log.Println("*************************************************************************")
+	// log.Println("Received Message is ", event.Text)
+	// log.Println("Event Type is ", event.Type)
+	// log.Println("User is ", event.User)
+	// log.Println("Channel is ", event.Channel)
+	// log.Println("*************************************************************************")
+
+	// client.PostMessage(event.Channel,slack.MsgOptionText("Bot Message : Let's Play Holi 😎",false))
+
 }
